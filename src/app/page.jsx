d702fcd24550c2f4661280e464e2e2e6bd058175
PaskahAbadi <email_paskahsimanullang24@gmail.com>
2025-12-15 15:26:26 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Page() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Praktik 14!</h1>
+      <p>Selamat datang di halaman utama Next.js saya.</p>
+    </div>
+  );
+}
